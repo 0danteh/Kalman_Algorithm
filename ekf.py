c10ee926b0847d003a3aafb612f607eb007357eb
verzade <francescopolymathes@gmail.com>
2023-11-17 23:59:47 +0100

    Update

--- ekf.py
+++ ekf.py
@@ -1,8 +1,9 @@
 import numpy as np
-from keract import sigmoid, sigmoid_prime
+
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 def sigmoid_prime(x):
     return sigmoid(x) * (1 - sigmoid(x))
+
