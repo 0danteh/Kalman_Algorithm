adda496bb13ff4b53bf1ed72cc2e5b2010209bab
verzade <francescopolymathes@gmail.com>
2023-11-18 00:25:39 +0100

    Update

--- ekf.py
+++ ekf.py
@@ -7,3 +7,12 @@ def sigmoid(x):
 def sigmoid_prime(x):
     return expit(x) * (1 - expit(x))
 
+class EKF_NN:
+    def __init__(self, n_input, n_hidden, n_output, Q, R):
+        # Initialize network parameters
+        self.n_input = n_input
+        self.n_hidden = n_hidden
+        self.n_output = n_output
+        self.n_weights = (n_input + 1) * n_hidden + (n_hidden + 1) * n_output
+        self.Q = Q # Process noise covariance
+        self.R = R # Measurement noise covariance
