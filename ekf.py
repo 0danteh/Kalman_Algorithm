50bc41d4bc1684c07ec63e49b8a9adde6502f88a
verzade <francescopolymathes@gmail.com>
2023-11-18 00:00:11 +0100

    Update

--- ekf.py
+++ ekf.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.special import expit, logit
 
 def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+    return expit(x)
 
 def sigmoid_prime(x):
     return sigmoid(x) * (1 - sigmoid(x))
