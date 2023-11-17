adf3ec980fde44687d039f3219da40eca5f0b785
verzade <francescopolymathes@gmail.com>
2023-11-18 00:49:07 +0100

    Update

--- ekf.py
+++ ekf.py
@@ -10,15 +10,15 @@ def sigmoid_prime(x):
 class EKF_NN:
     def __init__(self, n_input, n_hidden, n_output, Q, R):
         # Initialize network parameters
-        self.n_input = n_input
-        self.n_hidden = n_hidden
-        self.n_output = n_output
-        self.n_weights = (n_input + 1) * n_hidden + (n_hidden + 1) * n_output
-        self.Q = Q # Process noise covariance
-        self.R = R # Measurement noise covariance
+        self.n_input = n_input # Number of input nodes
+        self.n_hidden = n_hidden # Number of hidden nodes
+        self.n_output = n_output # Number of output nodes
+        self.n_weights = (n_input + 1) * n_hidden + (n_hidden + 1) * n_output # Total number of weights
+        self.Q = Q # Process noise covariance matrix (n_weights x n_weights)
+        self.R = R # Measurement noise covariance matrix (n_output x n_output)
 
         # Initialize network weights randomly
-        self.W1 = np.random.randn(n_input, n_hidden)
-        self.b1 = np.random.randn(n_hidden)
-        self.W2 = np.random.randn(n_hidden, n_output)
-        self.b2 = np.random.randn(n_output)
+        self.init_weights()
+
+        # Initialize error covariance matrix
+        self.P = np.eye(self.n_weights) * 1e-3 # Identity matrix with a small diagonal value
