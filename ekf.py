f18379f10fc73b8078d2ba332118a08f173c0f09
verzade <francescopolymathes@gmail.com>
2023-11-18 02:13:25 +0100

    Update

--- ekf.py
+++ ekf.py
@@ -18,11 +18,6 @@ class EKF_NN:
         self.Q = Q # Process noise covariance matrix (n_weights x n_weights)
         if not isinstance(Q, np.ndarray) or Q.shape != (self.n_weights, self.n_weights):
             raise ValueError("Invalid dimensions for Q matrix")
-        # Calculate the inverse of the process noise covariance matrix Q
-        try:
-            self.prior_cov = np.linalg.inv(Q)
-        except np.linalg.LinAlgError:
-            raise ValueError("Q matrix is not invertible")
         self.R = R # Measurement noise covariance matrix (n_output x n_output)
         if not isinstance(R, np.ndarray) or R.shape != (n_output, n_output):
             raise ValueError("Invalid dimensions for R matrix")
@@ -66,3 +61,20 @@ class EKF_NN:
                     for k in range(self.n_hidden):
                         H[i, j] += dy_dw[i, self.n_input + 1 + i * (self.n_hidden + 1) + k] * dh_dw[k, j] # H_ij = dy_i / dw_j + sum_k (dy_i / dw_k) * (dh_k / dw_j 
             return H
+        
+        def update(self, x, y):
+            # Update the network weights and the error covariance matrix based on the input and output vectors
+            y_pred = self.predict(x) # Predict the output of the network given the input vector
+            H = self.jacobian(x) # Compute the Jacobian matrix of the network output with respect to the weights
+            # Compute the innovation, innovation covariance, and Kalman gain matrices
+            nu = y - y_pred # Innovation vector (n_output x 1)
+            S = np.dot(np.dot(H, self.P), H.T) + self.R # Innovation covariance matrix (n_output x n_output)
+            try:
+                S_inv = np.linalg.inv(S) # Inverse of the innovation covariance matrix
+            except np.linalg.LinAlgError:
+                raise ValueError("S matrix is not invertible")
+            K = np.dot(np.dot(self.P, H.T), S_inv) # Kalman gain matrix (n_weights x n_output)
+            # Update the network weights and the error covariance matrix
+            self.w = self.w + np.dot(K, nu) # Weight update vector (n_weights x 1)
+            I = np.eye(self.n_weights) # Identity matrix (n_weights x n_weights)
+            self.P = np.dot(np.dot(I - np.dot(K, H), self.P), (I - np.dot(K, H)).T) + np.dot(np.dot(K, self.R), K.T) # Error covariance update matrix (n_weights x n_weights)
