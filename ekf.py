c7a27f93166ac846f89e7b415ab4764d2028e800
verzade <francescopolymathes@gmail.com>
2023-11-18 02:15:13 +0100

    Update

--- ekf.py
+++ ekf.py
@@ -1,12 +1,4 @@
 import numpy as np
-from scipy.special import expit, logit
-
-def sigmoid(x):
-    return expit(x)
-
-def sigmoid_prime(x):
-    return expit(x) * (1 - expit(x))
-
 
 class EKF_NN:
     def __init__(self, n_input, n_hidden, n_output, Q, R):
