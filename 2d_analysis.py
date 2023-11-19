22733ead610e1bf5e53a74c9f22cfb4d29c678bf
verzade <francescopolymathes@gmail.com>
2023-11-19 17:59:29 +0100

    Update

--- 2d_analysis.py
+++ 2d_analysis.py
@@ -1,2 +1,12 @@
 from sklearn.datasets import make_classification
 import numpy as np
+
+# Generate a random noisy synthetic dataset
+X, y = make_classification(
+    n_samples=1000,
+    n_features=2,
+    n_redundant=0,
+    n_clusters_per_class=1,
+    flip_y=0.15, # Noise level
+    random_state=42
+)
