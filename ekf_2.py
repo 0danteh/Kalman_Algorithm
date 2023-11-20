64baa3ce6286019a31309e530fcb840f9e6a9d39
verzade <francescopolymathes@gmail.com>
2023-11-20 16:58:58 +0100

    Update

--- ekf_2.py
+++ ekf_2.py
@@ -7,4 +7,18 @@ class EKF:
         self.n_input = int(n_input)
         self.n_output = int(n_output)
         self.n_hidden = int(n_hidden)
+
+        # Neuron type
+        self.neuron = neuron
+        if neuron == 'logistic':
+            self.sig = lambda V: 1 / (1 + np.exp(-V))
+            self.dsig = lambda sigV: sigV * (1 - sigV)
+        elif neuron == 'tanh':
+            self.sig = np.tanh
+            self.dsig = lambda sigV: 1 - sigV**2
+        elif neuron == 'relu':
+            self.sig = lambda V: np.maximum(V, 0)
+            self.dsig = lambda sigV: np.where(sigV > 0, 1, 0)
+        else:
+            raise ValueError("The neuron argument must be 'logistic', 'tanh', or 'relu'.")
     
