4f2faed2d85a0161e6c1b09995f381ca3cce9dcc
verzade <francescopolymathes@gmail.com>
2023-11-20 16:58:43 +0100

    Update

--- ekf_2.py
+++ ekf_2.py
@@ -1,4 +1,10 @@
 import numpy as np
 
 class EKF:
-    def __init__(self, nu, ny, nl, neuron, sprW=5):
+    def __init__(self, n_input, n_output, n_hidden, neuron, sprW=5):
+
+        # Function dimensionalities
+        self.n_input = int(n_input)
+        self.n_output = int(n_output)
+        self.n_hidden = int(n_hidden)
+    
