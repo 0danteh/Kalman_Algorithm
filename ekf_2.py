ee07a9e66171f46039dd1e6d3635d8bf996fb442
verzade <francescopolymathes@gmail.com>
2023-11-20 18:13:02 +0100

    Update

--- ekf_2.py
+++ ekf_2.py
@@ -37,20 +37,30 @@ class EKF:
     # Assigning labels
     def assign(self, train_input, hbound, lbound=0):
         return np.int64(np.minimum(np.maximum(self.update(train_input, 0), lbound, hbound)))
-    # Calculate the EKF
-    def ekf(self, train_input, train_output, h, l, step):
-        # Compute NN Jacobian
-        jacobian_D = np.multiply(self.W[1][:, :-1], self.dsig(l)).ravel()
-        jacobian_H = np.hstack((np.hstack((np.kron(jacobian_D, train_input), jacobian_D[:, np.newaxis])).reshape(self.n_output, self.W[0].size),
-                                np.repeat(np.concatenate((l, [1])), self.n_output).reshape(self.n_output, self.n_hidden+1)))
-        # Calculate Kalman gain
-        S = np.dot(jacobian_H, np.dot(self.P, jacobian_H.T)) + self.R
-        K = np.dot(np.dot(self.P, jacobian_H.T), np.linalg.inv(S))
-        # Update weight estimates and covariance
-        update_dW = step * np.dot(K, train_output - h)
-        self.W[0] += update_dW[:self.W[0].size].reshape(self.W[0].shape)
-        self.W[1] += update_dW[self.W[0].size:].reshape(self.W[1].shape)
-        self.P -= np.dot(K, np.dot(jacobian_H, self.P))
+    
+    def ekf_alt(self, train_input, train_output, h, l, step):
+
+        # Compute NN Jacobian using matrix multiplication
+        jacobian_D = self.W[1][:, :-1] * self.dsig(l)
+        jacobian_H = np.block([[np.kron(jacobian_D, train_input), jacobian_D], [l, 1]]).T
+        # Calculate Kalman gain using matrix inversion lemma
+        S_inv = np.linalg.inv(self.R) - np.linalg.inv(self.R + jacobian_H @ self.P @ jacobian_H.T) @ jacobian_H @ self.P
+        K = self.P @ jacobian_H.T @ S_inv
+        # Update weight estimates and covariance using matrix subtraction
+        update_dW = step * K @ (train_output - h)
+        self.W[0] -= update_dW[:self.W[0].size].reshape(self.W[0].shape)
+        self.W[1] -= update_dW[self.W[0].size:].reshape(self.W[1].shape)
+        self.P -= K @ jacobian_H @ self.P
         # Adjust covariance if Q is nonzero
         if self.Q_nonzero:
             self.P += self.Q
+
+    def sgd_alt(self, train_input, train_output, h, l, step):
+        # compute the error between the hidden layer output and the target output
+        error = np.subtract(h, train_output)
+        # update the weights of the second layer by subtracting the product of the error, the learning rate, and the hidden layer output with a bias term
+        self.W[1] = np.subtract(self.W[1], np.multiply(step, np.hstack((np.matmul(error, l.T), error[:, np.newaxis]))))
+        # compute the delta term for the first layer by multiplying the error, the weights of the second layer without the bias term, and the derivative of the sigmoid function applied to the hidden layer output
+        delta = np.multiply(np.matmul(error, self.W[1][:, :-1]), self.dsig(l)).flatten()
+        # update the weights of the first layer by subtracting the product of the delta, the learning rate, and the input layer output with a bias term
+        self.W[0] = np.subtract(self.W[0], np.multiply(step, np.hstack((np.matmul(delta[:, np.newaxis], train_input.T), delta[:, np.newaxis]))))
