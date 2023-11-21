2e55210e39c4fa97e129b2e093fc62ca9350cb39
verzade <francescopolymathes@gmail.com>
2023-11-21 12:48:48 +0100

    Update

--- ekf_2.py
+++ ekf_2.py
@@ -98,3 +98,4 @@ class EKF:
                 self.update(train_input, train_output, h, l, step)
                 if method == 'ekf': cov.append(np.trace(self.P))
                 if (time_tres >= 0 and time() - last_drwdwn > time_tres) or (epoch == epochs - 1 and i == len(train_input) - 1):
+                    print(f"RMSE: {np.sqrt(mean_squared_error(np.vstack(y_true_all), np.vstack(y_pred_all)))}")
