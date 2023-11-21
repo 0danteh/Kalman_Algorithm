757837d9affe9af77f3b1e97f029c1945ea322d2
verzade <francescopolymathes@gmail.com>
2023-11-21 12:55:53 +0100

    Update

--- sim2.py
+++ sim2.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from ekf_2 import EKF
 
 # Set the random seed for reproducibility
 np.random.seed(42)
@@ -18,4 +19,5 @@ f = lambda x: np.sin(2 * np.pi * x[:, 0]) * np.cos(2 * np.pi * x[:, 1])
 # Generate the output feature by adding some Gaussian noise to the function
 y = f(X) + np.random.normal(0, 0.1, size=(n_samples, n_output))
 
-
+# Create an instance of the EKF class with 2 input features, 1 output feature, 10 hidden units, and tanh activation functio
+ekf = EKF(n_input=2, n_output=1, n_hidden=10, neuron='tanh')
