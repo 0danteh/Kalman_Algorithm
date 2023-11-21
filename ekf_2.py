e92ffbcbe88be2e23b4db0e3b8c585bfb103247a
verzade <francescopolymathes@gmail.com>
2023-11-21 14:30:44 +0100

    Update

--- ekf_2.py
+++ ekf_2.py
@@ -3,22 +3,22 @@ from time import time
 from sklearn.metrics import mean_squared_error
 
 class EKF:
-    def __init__(self, n_input, n_output, n_hidden, neuron, sprW=5):
+    def __init__(self, n_input, n_output, n_hidden, act_fun, sprW=5):
 
         # Function dimensionalities
         self.n_input = int(n_input)
         self.n_output = int(n_output)
         self.n_hidden = int(n_hidden)
         # Neuron type
-        self.neuron = neuron
-        if neuron == 'logistic':
+        self.act_fun = act_fun
+        if act_fun == 'logistic':
             self.sig = lambda V: 1 / (1 + np.exp(-V))
             self.dsig = lambda sigV: sigV * (1 - sigV)
-        elif neuron == 'tanh':
+        elif act_fun == 'tanh':
             self.sig = np.tanh
             self.dsig = lambda sigV: 1 - sigV**2
-        elif neuron == 'relu':
-            self.sig = lambda V: np.maximum(V, 0)
+        elif act_fun == 'relu':
+            self.sig = lambda V: np.clip(V, 0, np.inf)
             self.dsig = lambda sigV: np.where(sigV > 0, 1, 0)
         else:
             raise ValueError("The neuron argument must be 'logistic', 'tanh', or 'relu'.")
