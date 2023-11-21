9961ebe3350d31039673ba635574c43bf9c7f734
verzade <francescopolymathes@gmail.com>
2023-11-21 12:45:36 +0100

    Update

--- ekf_2.py
+++ ekf_2.py
@@ -80,3 +80,4 @@ class EKF:
             self.feed = self.ekf_alt
             self.P = P*np.eye(self.nW, dtype=np.float64)
             self.Q = np.zeros((self.nW, self.nW), dtype=np.float64) if Q is None else np.float64(Q)
+            self.Q_nonzero = np.any(self.Q)
