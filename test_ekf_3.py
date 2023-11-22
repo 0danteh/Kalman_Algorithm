636a60e7c16a0fd46dbdc2d1158ec229f208d214
verzade <francescopolymathes@gmail.com>
2023-11-22 23:26:32 +0100

    Update

--- test_ekf_3.py
+++ test_ekf_3.py
@@ -52,4 +52,4 @@ class KF_EKF:
         # Create a list of two arrays with random weights
         self.W = [random_weights((n_hidden, n_input+1)), random_weights((n_output,n_hidden+1))]
         self.num_weights = sum([np.size(w) for w in self.W])
-        
+        self.P = None
