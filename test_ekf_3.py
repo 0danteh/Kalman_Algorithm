b0524b5a752285e1ddfabe1110881986c9053e98
verzade <francescopolymathes@gmail.com>
2023-11-22 23:40:33 +0100

    Update

--- test_ekf_3.py
+++ test_ekf_3.py
@@ -79,3 +79,4 @@ class KF_EKF:
         self.W[0]=self.W[0]+dW[:self.W[0].size].reshape(self.W[0].shape)
         self.W[1]=self.W[1]+dW[self.W[0].size].reshape(self.W[1].shape)
         self.P=self.P-np.dot(K,self.H.dot(self.P))
+        if self.Q_nonzero: self.P=self.P+self.Q
