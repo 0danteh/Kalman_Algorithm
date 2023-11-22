2d747ca3680a9616ca7b630f72cb3550a8803fad
verzade <francescopolymathes@gmail.com>
2023-11-22 23:24:24 +0100

    Update

--- test_ekf_3.py
+++ test_ekf_3.py
@@ -50,3 +50,5 @@ class KF_EKF:
         # Inilialising weight matrix
         weight_scale = np.float64(weight_scale)
         # Create a list of two arrays with random weights
+        self.W = [random_weights((n_hidden, n_input+1)), random_weights((n_output,n_hidden+1))]
+    
