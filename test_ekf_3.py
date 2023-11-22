9ef3fce296b5c363db8c713d3c538f70beb0ffa2
verzade <francescopolymathes@gmail.com>
2023-11-22 23:22:07 +0100

    Update

--- test_ekf_3.py
+++ test_ekf_3.py
@@ -43,3 +43,7 @@ class KF_EKF:
         self.activ=self.activ
         if activ not in ['logistic', 'tanh', 'relu']:
             raise ValueError("The 'activ' argument must be 'logistic', 'tanh', or 'relu'.")
+
+        # Random arrays with diff shapes
+        def random_weights(shape):
+            return weight_scale*(2*np.random.sample(shape)-1)
