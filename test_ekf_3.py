6ca51cfebee0d6561eca3e4b9612fac8b13f09d4
verzade <francescopolymathes@gmail.com>
2023-11-22 23:13:56 +0100

    Update

--- test_ekf_3.py
+++ test_ekf_3.py
@@ -20,3 +20,6 @@ def _check_matrix(M,n,error_msg):
 class KF_EKF:
     def __init__(self, n_input, n_output, n_hidden, activ, weight_scale=5):
         
+        self.n_input=int(n_input)
+        self.n_output=int(n_output)
+        self.n_hidden=int(n_hidden)
