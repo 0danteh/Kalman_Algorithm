b2a5ca0c1d59f9eab7e529ef287098ec7dd84c3f
verzade <francescopolymathes@gmail.com>
2023-11-23 23:46:59 +0100

    Update

--- def_ekf.py
+++ def_ekf.py
@@ -137,4 +137,4 @@ class KF_EKF:
             train_output_shuffled=Y[shuffl]
             pbar=tqdm(train_input_shuffled, desc=f"Epoch {epoch+1}/{n_epochs}", 
                       unit="batch", bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}]")
-
+            # Update the neural networks and train
