961ffc00e7dc52feec12ab05bda7eae5682b363d
verzade <francescopolymathes@gmail.com>
2023-11-24 01:29:13 +0100

    Update

--- def_ekf.py
+++ def_ekf.py
@@ -49,45 +49,45 @@ class EKF:
 
     def __init__(self,n_input,n_output,n_hidden,activ,weight_scale=5):
         
-        self.n_input = int(n_input)
-        self.n_output = int(n_output)
-        self.n_hidden = int(n_hidden)
-        self.activ = activ
+        self.n_input=int(n_input)
+        self.n_output=int(n_output)
+        self.n_hidden=int(n_hidden)
+        self.activ=activ
         if activ not in ['logistic', 'tanh', 'relu']:
             raise ValueError("The 'activ' argument must be 'logistic', 'tanh', or 'relu'.")
         # Random arrays with diff shapes
         def random_weights(shape):
             return weight_scale*(2*np.random.sample(shape)-1)
         # Inilialising weight matrix
-        weight_scale = np.float64(weight_scale)
+        weight_scale=np.float64(weight_scale)
         # Create a list of two arrays with random weights
-        self.W = [random_weights((n_hidden,n_input+1)),random_weights((n_output,n_hidden+1))]
-        self.num_weights = sum([np.size(w) for w in self.W])
-        self.P = None
-        self.affine_transform = lambda W,V: np.dot(np.atleast_1d(V), W[:, :-1].T) + W[:, -1]
+        self.W=[random_weights((n_hidden,n_input+1)),random_weights((n_output,n_hidden+1))]
+        self.num_weights=sum([np.size(w) for w in self.W])
+        self.P=None
+        self.affine_transform=lambda W,V: np.dot(np.atleast_1d(V), W[:, :-1].T) + W[:, -1]
 
     # Feeding the neural network
     def update(self,U,return_l=False):
-        U = np.float64(U)
+        U=np.float64(U)
         if U.ndim == 1 and len(U) > self.n_input:
-            U = U[:, np.newaxis]
-        l = self.sig(self.affine_transform(self.W[0], U))
-        h = self.affine_transform(self.W[1], l)
+            U=U[:, np.newaxis]
+        l=self.sig(self.affine_transform(self.W[0], U))
+        h=self.affine_transform(self.W[1], l)
         return (h, l) if return_l else h
     
     # Calculate the jacobian for the EKF algorithm
     def jacobian(self, u, l):
         # Compute NN jacobian
-        D = (self.W[1][:, :-1]*self.dsig(l)).flatten()
-        H = np.hstack((outer_plus_bias(D, u).reshape(self.n_output, self.W[0].size), block_diag(*np.tile(np.concatenate((l, [1])), self.n_output).reshape(self.n_output, self.n_hidden+1))))
+        D=(self.W[1][:, :-1]*self.dsig(l)).flatten()
+        H=np.hstack((outer_plus_bias(D, u).reshape(self.n_output, self.W[0].size), block_diag(*np.tile(np.concatenate((l, [1])), self.n_output).reshape(self.n_output, self.n_hidden+1))))
         return H
     
     def update_weights_and_cov(self, K, dW):
         # Update weight estimates and covariance
-        self.W[0] = self.W[0] + dW[:self.W[0].size].reshape(self.W[0].shape)
-        self.W[1] = self.W[1] + dW[self.W[0].size:].reshape(self.W[1].shape)
-        self.P = self.P - np.dot(K, self.H.dot(self.P))
-        if self.Q_nonzero: self.P = self.P + self.Q
+        self.W[0]=self.W[0]+dW[:self.W[0].size].reshape(self.W[0].shape)
+        self.W[1]=self.W[1]+dW[self.W[0].size:].reshape(self.W[1].shape)
+        self.P=self.P-np.dot(K, self.H.dot(self.P))
+        if self.Q_nonzero: self.P=self.P+self.Q
 
     # Compute the kalman gain
     def kalman_gain(self, P, H, R):
@@ -96,38 +96,38 @@ class EKF:
     
     def _ekf(self, u, y, h, l, step):
         # Update the network
-        self.H = self.jacobian(u,l)
+        self.H=self.jacobian(u,l)
         # Kalman gain
-        K = self.kalman_gain(self.P, self.H, self.R)
+        K=self.kalman_gain(self.P, self.H, self.R)
         # Update weight estimates and covariance
-        dW = step*K.dot(y-h)
+        dW=step*K.dot(y-h)
         self.update_weights_and_cov(K, dW)
 
     def train(self, n_epochs, U, Y, method='ekf', P=None, Q=None, R=None, step=1):
-        U = np.float64(U)
-        Y = np.float64(Y)
-        U,Y = validate_shape(U,Y,self.n_input,self.n_output)
+        U=np.float64(U)
+        Y=np.float64(Y)
+        U,Y=validate_shape(U,Y,self.n_input,self.n_output)
         # Initialise EKF
         self.feed = self._ekf
         if P is None:
             if self.P is None:
                 raise ValueError("Initial P not specified.")
         else:
-            self.P = _check_matrix(P, self.num_weights, "P must be a float scalar or (num_weights by num_weights) array.")
-        self.Q = _check_matrix(Q, self.num_weights, "Q must be a float scalar or (num_weights by num_weights) array.")
-        if np.any(self.Q): self.Q_nonzero = True
-        else: self.Q_nonzero = False
-        self.R = _check_matrix(R, self.n_output, "R must be a float scalar or (n_output by n_output) array.")
+            self.P=_check_matrix(P, self.num_weights, "P must be a float scalar or (num_weights by num_weights) array.")
+        self.Q=_check_matrix(Q, self.num_weights, "Q must be a float scalar or (num_weights by num_weights) array.")
+        if np.any(self.Q): self.Q_nonzero=True
+        else: self.Q_nonzero=False
+        self.R=_check_matrix(R, self.n_output, "R must be a float scalar or (n_output by n_output) array.")
         if npl.matrix_rank(self.R) != len(self.R):
             raise ValueError("R must be positive definite.")
 
         # Start the training
         for epoch in range(n_epochs):
-            shuffl = np.random.permutation(len(U))
-            train_input_shuffled = U[shuffl]
-            train_output_shuffled = Y[shuffl]
+            shuffl=np.random.permutation(len(U))
+            train_input_shuffled=U[shuffl]
+            train_output_shuffled=Y[shuffl]
             pbar = tqdm(train_input_shuffled, desc=f"Epoch {epoch + 1}/{n_epochs}", unit="batch", bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}]")
             # Update the neural networks and train
-            for i, (u, y) in enumerate(zip(pbar, train_output_shuffled)):
-                h, l = self.update(u, return_l=True)
+            for i, (u,y) in enumerate(zip(pbar, train_output_shuffled)):
+                h, l=self.update(u, return_l=True)
                 self.feed(u, y, h, l, step)
