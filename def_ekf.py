fa926faea8fdb2a692c7fc43f83ea78f35e09748
verzade <francescopolymathes@gmail.com>
2023-11-24 01:25:56 +0100

    Update

--- def_ekf.py
+++ def_ekf.py
@@ -126,9 +126,8 @@ class EKF:
             shuffl = np.random.permutation(len(U))
             train_input_shuffled = U[shuffl]
             train_output_shuffled = Y[shuffl]
-            pbar=tqdm(train_input_shuffled, desc=f"Epoch {epoch+1}/{n_epochs}", 
-                      unit="batch", bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}]")
+            pbar = tqdm(train_input_shuffled, desc=f"Epoch {epoch + 1}/{n_epochs}", unit="batch", bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}]")
             # Update the neural networks and train
-            for i, (u,y) in enumerate(zip(pbar, train_output_shuffled)):
-                h,l=self.update(u,return_l=True)
-                self.feed(u,y,h,l,step)
+            for i, (u, y) in enumerate(zip(pbar, train_output_shuffled)):
+                h, l = self.update(u, return_l=True)
+                self.feed(u, y, h, l, step)
