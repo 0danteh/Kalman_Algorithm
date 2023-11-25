8d7b98329fd4211c8adf07f0461107939007fc21
verzade <francescopolymathes@gmail.com>
2023-11-26 00:19:22 +0100

    Update

--- double_pendulum_simulation.py
+++ double_pendulum_simulation.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.integrate import solve_ivp
 from sklearn.preprocessing import StandardScaler
 from def_ekf import EKF
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 # Define the double pendulum dynamics 
 def double_pendulum(t,y):  
