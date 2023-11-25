6faa810834f5bef378e07c5c890b2220267d3428
verzade <francescopolymathes@gmail.com>
2023-11-25 19:44:22 +0100

    Update

--- double_pendulum_simulation.py
+++ double_pendulum_simulation.py
@@ -5,4 +5,14 @@ from def_ekf import EKF
 import matplotlib as plt
 
 # Define the double pendulum dynamics. 
-def double_pendulum(t,y):
+def double_pendulum(t,y):  
+    """
+    Parameters:
+    - m = mass
+    - l = length
+    - g = gravity acceleration
+    Note: since it's about a double nonlinear pendulum system, number-based labeling conventions
+          are used for further distinction of the variables.
+    """
+
+    
